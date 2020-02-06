e001822f9b209a6fe5f9e1a7b3f88ffc6419307e
Alex <alexxsur@gmail.com>
2020-02-05 22:16:36 -0500

    Initial Commit

---
+++ js/generic.js
@@ -0,0 +1,13 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+      apiKey: "AIzaSyBA56aEa3EOLBklcq_Yu4bIT4FQ8yfxJOQ",
+      authDomain: "miprimerproyectoconfireb-25554.firebaseapp.com",
+      databaseURL: "https://miprimerproyectoconfireb-25554.firebaseio.com",
+      projectId: "miprimerproyectoconfireb-25554",
+      storageBucket: "miprimerproyectoconfireb-25554.appspot.com",
+      messagingSenderId: "463869648612",
+      appId: "1:463869648612:web:c148e6e59ab5e61f7056f1",
+      measurementId: "G-6F79PTW7TJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
