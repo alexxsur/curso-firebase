e70fa170d33aa52f32bba50ada330d48a3be2b30
Alex <alexxsur@gmail.com>
2020-02-08 11:23:22 -0500

    Inicio de sesion con firebase

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+function iniciarSesion() {
+    var email = document.getElementById("txtcorreoIngresar").value;
+    var password = document.getElementById("txtcontraIngresar").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(res => {
+
+            document.location.href = "misPrestamos.html";
+            // IMAGEN
+            if (res.user.photoURL != null) {
+                document.getElementById("imgFotoUsuario").src = res.user.photoURL;
+            } else {
+                document.getElementById("imgFotoUsuario").src = "/img/noFoto.jpeg";
+            }
+        }).catch(err => {
+            document.getElementById("alertErrorLogueo").style.display = "block";
+            document.getElementById("alertErrorLogueo").innerHTML = err;
+        })
+}
+
 function createUser() {
     var email = document.getElementById("txtcorreo").value;
     var password = document.getElementById("txtcontra").value;
