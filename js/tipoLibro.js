0e9f0dd28009fedd240ce206c252f2b13800ce89
Alex <alexxsur@gmail.com>
2020-02-09 10:06:13 -0500

    Listado de datos entiempo real

--- js/tipoLibro.js
+++ js/tipoLibro.js
@@ -1,4 +1,46 @@
 window.onload = function() {
     this.verAutenticacion();
-    console.log('TIPOLIBRO');
+
+    firebase.firestore().collection("TipoLibro").where("bhabilitado", "==", "1")
+        .onSnapshot(res => {
+            listarTipoLibros(res);
+        });
+}
+
+function listarTipoLibros(res) {
+    var contenido = "<table class='table mt-2'";
+    contenido += "<thead>";
+
+    contenido += "<tr>";
+
+    contenido += "<th>Id</th>";
+    contenido += "<th>Nombre Tipo Libro</th>";
+    contenido += "<th>Descripción</th>";
+    contenido += "<th>Operaciones</th>";
+
+    contenido += "</tr>";
+
+    contenido += "</thead>";
+
+    contenido += "<tbody>";
+
+
+    res.forEach(rpta => {
+
+        var fila = rpta.data();
+        contenido += "<tr>";
+
+        contenido += "<td>" + fila.id + "</td>";
+        contenido += "<td>" + fila.nombre + "</td>";
+        contenido += "<td>" + fila.descripcion + "</td>";
+        contenido += "<td></td>";
+
+        contenido += "</tr>";
+
+    });
+
+    contenido += "</tbody>";
+
+    contenido += "</table>";
+    document.getElementById("divTipoLibro").innerHTML = contenido;
 }
