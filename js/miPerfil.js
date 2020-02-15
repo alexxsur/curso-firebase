76457262fe70ba72b3fd3ecb200393a41bb11f23
Alex <alexxsur@gmail.com>
2020-02-15 13:43:36 -0500

    Recuperar Información de Firebase Perfil de Usuario

---
+++ js/miPerfil.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    this.verAutenticacion();
+    firebase.auth().onAuthStateChanged(res => {
+        cargarPerfil();
+    });
+}
+
+var user;
+
+function cargarPerfil() {
+    user = firebase.auth().currentUser.uid;
+    firebase.firestore().collection("Usuarios").doc(user)
+        .get().then(resultado => {
+            var res = resultado.data();
+            document.getElementById("txtDisplayName").value = res.displayName;
+            document.getElementById("txtNombre").value = res.nombre;
+            document.getElementById("txtApellido").value = res.apellido;
+            document.getElementById("txtEmail").value = res.email;
+            document.getElementById("txtTelefono").value = res.phoneNumber;
+            document.getElementById("txtDescripcion").value = res.descripcion != undefined ? res.descripcion : "";
+            document.getElementById("imgFoto").src = res.photoURL;
+        }).catch(err => {
+            alert(err);
+        });
+}
