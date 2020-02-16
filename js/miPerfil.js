32714a32724c89bdc1b076520b009439a02c2436
Alex <alexxsur@gmail.com>
2020-02-16 12:21:41 -0500

    Guardar fotos en storage y actualizar url en firestore

--- js/miPerfil.js
+++ js/miPerfil.js
@@ -24,6 +24,52 @@ function cargarPerfil() {
         });
 }
 
+function editarPerfil() {
+    var displayName = document.getElementById("txtDisplayName").value;
+    var nombre = document.getElementById("txtNombre").value;
+    var apellido = document.getElementById("txtApellido").value;
+    var email = document.getElementById("txtEmail").value;
+    var phoneNumber = document.getElementById("txtTelefono").value;
+    var descripcion = document.getElementById("txtDescripcion").value;
+
+    var photoURL = document.getElementById("imgFoto").src;
+    firebase.firestore().collection("Usuarios").doc(user).update({
+        displayName: displayName,
+        nombre: nombre,
+        apellido: apellido,
+        email: email,
+        phoneNumber: phoneNumber,
+        descripcion: descripcion,
+    }).then(res => {
+        var objFoto = document.getElementById("foto");
+        var foto = objFoto.value; // La ruta del archivo seleccionado
+
+        if (foto != null && foto != "") {
+
+            var ref = firebase.storage().ref("fotoPerfil/" + user + "/" + objFoto.files[0].name);
+            var archivo = objFoto.files[0];
+            var refFoto = ref.put(archivo);
+            refFoto.on("state_changed", () => {}, (err) => { alert(err) }, () => {
+                refFoto.snapshot.ref.getDownloadURL().then(url => {
+                    firebase.firestore().collection("Usuarios").doc(user).update({
+                        photoURL: url
+                    }).then(respuesta => {
+                        alert("Se actualizó correctamente");
+                    }).catch(err => {
+                        alert(err);
+                    })
+                })
+            })
+
+        } else {
+            alert("Se editó correctamente");
+        }
+
+    }).catch(err => {
+        alert(err);
+    })
+}
+
 function cambiarFoto(archivo) {
     var file = archivo.files[0];
     var reader = new FileReader();
